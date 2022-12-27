21f5357e9459f5a81bb05b92c74c84e88763e4be
grribs <zamaruev@gmail.com>
2022-12-27 13:13:00 +0700

    sync 27.12 C1 OOP SF

--- test_.py
+++ test_.py
@@ -1,9 +1,23 @@
-#7 подъезд 462 квартира 7 эт дом / этаж?
-
-
-
-
 
 # a = 1234314465479
 # d = a % 10
 # print(d)
+
+
+class Person:
+    def print_info(self, n):
+        for i in range(n):
+            print(f"Name: {self.name}, Surname: {self.surname}, Place of birth: {self.place_of_birth}")
+
+    def get_age(self, age):
+        return print(age - self.year_of_birth)
+
+
+p1 = Person()
+p1.name = "Elon"
+p1.surname = "Musk"
+p1.place_of_birth = "ЮАР"
+p1.year_of_birth = 1971
+
+
+p1.get_age(2020)
